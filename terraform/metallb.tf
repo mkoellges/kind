8cbd3cd158eb1b3fb8e7ca9a5b9352baf44a11cf
Manfred Köllges <manfredkollges@me.com>
2023-12-29 14:27:56 +0100

    Revert "updated metallb in terraform"
    This reverts commit 5e9a7141e102e5fc23b9569a97fa0a45e203d9f1.

--- terraform/metallb.tf
+++ terraform/metallb.tf
@@ -3,6 +3,7 @@ provider "kubectl" {
   client_key             = base64decode(kind_cluster.default.cluster.client_key)
   cluster_ca_certificate = base64decode(kind_cluster.default.cluster_ca_certificate)
   client_certificate     = base64decode(kind_cluster.default.client_certificate)
+
 }
 
 data "kubectl_path_documents" "docs" {
