525163dc56a74353e9b4f05ef484ee782dd376f2
Manfred Koellges <manfred.koellges@she.net>
2024-07-19 07:36:19 +0200

    change to local metallb manifest

--- terraform/application-metallb.tf
+++ terraform/application-metallb.tf
@@ -2,26 +2,13 @@ provider "kubernetes" {
   config_path = kind_cluster.default.kubeconfig_path
 }
 
-data "http" "metallb_manifest" {
-  url = "https://raw.githubusercontent.com/metallb/metallb/v0.14.7/config/manifests/metallb-native.yaml"
-}
-
 resource "null_resource" "metallb_install" {
   depends_on = [kind_cluster.default]
   
   provisioner "local-exec" {
-    command = "kubectl apply -f ${data.http.metallb_manifest.url}"
+    command = "kubectl apply -f ./manifests/metallb-native.yaml"
   }
 }
-
-resource "null_resource" "metallb_svc_patch" {
-  depends_on = [null_resource.metallb_install]
-
-  provisioner "local-exec" {
-    command = "kubectl patch svc metallb-webhook-service -n metallb-system -p '{\"spec\": {\"type\": \"LoadBalancer\"}}'"
-  }
-}
-
 resource "null_resource" "metallb_config" {
   depends_on = [null_resource.metallb_install]
 
