e918535b67c8f407452fcda11a0706145ffbdf87
Manfred Köllges <manfred.koellges@she.net>
2025-02-24 10:29:01 +0100

    fix: update to kind proxy

--- terraform/application-argocd.tf
+++ terraform/application-argocd.tf
@@ -6,13 +6,13 @@ provider "helm" {
 }
 
 resource "helm_release" "argocd" {
-  depends_on       = [null_resource.metallb_config]
+  depends_on       = [kind_cluster.default]
   name             = "argocd"
   namespace        = "argocd"
   create_namespace = true
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.52.0"
+  version          = "7.7.5"
 
   values = [
     "${file("./helm/argocd-values.yaml")}"
